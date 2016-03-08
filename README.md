5b5508c6669573f74cd1c7d4e150271c122277a7
abbottmb12 <abbottmb12@gmail.com>
2016-03-08 17:58:16 -0600

    Initial website

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # PizzaKingApp
 Pizza King Web Service
-brad was here
-brad was here again Brad was always here...
---
+++ scripts/order.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('#modal1').openModal();
+  //$('.modal-trigger').leanModal();
+});
