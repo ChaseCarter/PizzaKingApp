12d666a3f387e4b04f4d5fc28eb613b48c250727
abbottmb12 <abbottmb12@gmail.com>
2016-03-09 13:28:39 -0600

    Data page initial creation

--- scripts/data.js
+++ scripts/data.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
     $('select').material_select();
+	setData();
+	
 });
+
+var overallPizzas=["No Data"];
+var harryPizzas=["No Data"];
+var cicilianPizzas=["No Data"];
+
+function setData(){
+	for(var i=0;i<overallPizzas.length;i++){
+		$('#overallPopular.collection').append('<li class="collection-item">'+overallPizzas[i]+'</li>');
+	}
+	for(var i=0;i<harryPizzas.length;i++){
+		$('#harryPopular.collection').append('<li class="collection-item">'+harryPizzas[i]+'</li>');
+	}
+	for(var i=0;i<cicilianPizzas.length;i++){
+		$('#cicilianPopular.collection').append('<li class="collection-item">'+cicilianPizzas[i]+'</li>');
+	}
+}
--- scripts/order.js
+++ scripts/order.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 		subtotals = 0;
 		$('#priceSubtotals').empty();
 		$('#modalText').empty();
-		$('.totalPrice').empty();
+		$('.totalPrice').append().text("Total: $0.00");
 	});
 	
 	$("#pizzaSelect").change(function () {
@@ -37,10 +37,15 @@ $(document).ready(function() {
 		$('#modalText').append("<br>" + menu.Stores[store].Menu[pizza].PizzaName + ": " + price);
 		subtotals += price;
 		$('.totalPrice').empty();
-		$('.totalPrice').append(subtotals);
+		$('.totalPrice').append("Total: $" + precise_round(subtotals, 2));
 		console.log(subtotals);
 	});
 });
+
+function precise_round(num, decimals) {
+var t=Math.pow(10, decimals);   
+ return (Math.round((num * t) + (decimals>0?1:0)*(Math.sign(num) * (10 / Math.pow(100, decimals)))) / t).toFixed(decimals);
+    }
 
 var subtotals = 0;
 var menu = jQuery.parseJSON( '{"Stores": [{"StoreName":"Little Roman\'s","Menu": [{"PizzaName":"Cheese", "Price":8.99},{"PizzaName":"Pepperoni", "Price":10.99},{"PizzaName":"Ultimate", "Price":12.99}]},{"StoreName":"Poker Cards","Menu": [{"PizzaName":"Cheese", "Price":8.99},{"PizzaName":"Pepperoni", "Price":10.99},{"PizzaName":"Ultimate", "Price":12.99},{"PizzaName":"Spicy", "Price":11.99},{"PizzaName":"Meatser", "Price":12.99}]},{"StoreName":"The Cicilian\'s","Menu": [{"PizzaName":"Funky Medina", "Price":9.99},{"PizzaName":"Big Green", "Price":9.99},{"PizzaName":"Meat", "Price":8.99},{"PizzaName":"Spicy Meat", "Price":9.99},{"PizzaName":"Stinky", "Price":10.99}]},{"StoreName":"Mike\'s Pies","Menu": [{"PizzaName":"Steak", "Price":11.99},{"PizzaName":"Steak and Cheese", "Price":13.99},{"PizzaName":"Steak and Cheese and Veggies", "Price":14.99}]},{"StoreName":"Hoang\'s Pizzeria","Menu": [{"PizzaName":"Cheese", "Price":7.99},{"PizzaName":"Fish", "Price":9.99},{"PizzaName":"Ultimate Fish", "Price":11.99}]},{"StoreName":"Harry Gary\'s Pizza Pie","Menu": [{"PizzaName":"Harry Gary\'s Pie", "Price":10.99}]},{"StoreName":"Tiny Pizzas","Menu": [{"PizzaName":"Cheesey!", "Price":5.99},{"PizzaName":"Red!", "Price":6.99},{"PizzaName":"Spicy Oink!", "Price":8.99},{"PizzaName":"Oink!", "Price":6.99},{"PizzaName":"Fungi!", "Price":7.99},{"PizzaName":"Tangy!", "Price":6.99},{"PizzaName":"Spicy!", "Price":6.99},{"PizzaName":"Sweet!", "Price":8.99}]},{"StoreName":"Bad JuJu\'s Pizzas","Menu": [{"PizzaName":"Good", "Price":10.99},{"PizzaName":"Bad", "Price":12.99},{"PizzaName":"Ugly", "Price":14.99}]}]}' );
